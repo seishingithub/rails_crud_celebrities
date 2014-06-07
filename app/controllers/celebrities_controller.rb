f93d186cbf297a045d58db6a9e0a2818b11e6fa4
Peggy Griffin <seishingithub@gmail.com>
2014-06-07 12:23:52 -0600

    User can edit and update a celebrity

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -14,6 +14,20 @@ class CelebritiesController < ApplicationController
     end
   end
 
+  def show
+    @celebrity = Celebrity.find(params[:id])
+  end
+
+  def edit
+    @celebrity = Celebrity.find(params[:id])
+  end
+
+  def update
+    @celebrity = Celebrity.find(params[:id])
+    @celebrity.update_attributes!(celebrity_params)
+    redirect_to celebrities_path
+  end
+
   private
   def celebrity_params
     params.require(:celebrity).permit(:name, :job)
--- spec/features/celebrities_spec.rb
+++ spec/features/celebrities_spec.rb
@@ -11,4 +11,23 @@ feature 'Manage celebrities' do
     expect(page).to have_content 'Tom Cruise'
     expect(page).to have_content 'actor'
   end
+
+  scenario 'User can edit and update celebrities' do
+    visit '/'
+    click_on 'Add a celebrity'
+    fill_in 'Name', with: 'Tom Cruise'
+    fill_in 'Job', with: 'actor'
+    click_on 'Add celebrity'
+    expect(page).to have_content 'Tom Cruise'
+    expect(page).to have_content 'actor'
+    click_on 'Tom Cruise'
+    expect(page).to have_content 'Tom Cruise'
+    expect(page).to have_content 'actor'
+    click_on 'Edit'
+    fill_in 'Name', with: 'Shirley Temple'
+    fill_in 'Job', with: 'actress'
+    click_on 'Update celebrity'
+    expect(page).to have_content 'Shirley Temple'
+    expect(page).to have_content 'actress'
+  end
 end
